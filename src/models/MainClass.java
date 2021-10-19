26d79d42e9b0ea138af8ec79c7e1fd20d0145bb1
Alejo Medina <edgar.medina01@uptc.edu.co>
2021-10-19 13:05:40 -0500

    Este es el segundo commit

--- src/models/MainClass.java
+++ src/models/MainClass.java
@@ -4,5 +4,9 @@ public class MainClass {
 
     public static void main(String[] args) {
         System.out.printf("Hola mundo");
+        int x = 10;
+        int y = 15;
+        int suma = x + y;
+        System.out.printf("Resultado: " + suma);
     }
 }
