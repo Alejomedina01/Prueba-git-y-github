1758beea3955f014d00feab3b2193de56f223c78
Alejo Medina <edgar.medina01@uptc.edu.co>
2021-10-22 11:00:43 -0500

    Se añadio el paquete y la clase controlador

---
+++ src/controllers/Controller.java
@@ -0,0 +1,9 @@
+package controllers;
+
+public class Controller {
+    private MainClass modelo;
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/models/MainClass.java
+++ src/models/MainClass.java
@@ -2,11 +2,5 @@ package models;
 
 public class MainClass {
 
-    public static void main(String[] args) {
-        System.out.printf("Hola mundo");
-        int x = 10;
-        int y = 15;
-        int suma = x + y;
-        System.out.printf("Resultado: " + suma);
-    }
+
 }
